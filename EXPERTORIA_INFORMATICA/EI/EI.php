6a3824d3645dd4c1a943525ff42a8d9b6fed077a
DESTROLLER616 <104311024+DESTROLLER616@users.noreply.github.com>
2022-06-26 23:00:55 -0500

    Add files via upload

---
+++ EXPERTORIA_INFORMATICA/EI/EI.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Pagina principal</title>
+    <link rel="shortcut icon" href="../../IMG/Expertoria Informatica logo.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="icon" href="./logo.png">
+    <link rel="stylesheet" href="../../CSS/Estilo.css">
+</head>
+<body>
+    <div class="container">
+        <div class="lbl-menu">
+            <!-- etiquetas del menú -->
+            <label for="radio1">SOBRE NOSOTROS</label>
+            <label for="radio2">CREADORES</label>
+            <label for="radio3">PROBLEMATICA</label>
+            <label for="radio4">DOCUMENTOS</label>
+            <label for="radio5">CONTACTO</label>
+        </div>
+        
+        <div class="content">
+            <input type="radio" name="radio" id="radio1" checked>
+            <div class="tab1">
+                <!-- imagen de la empresa -->
+                <img src="../../IMG/logo.png" alt="logo" class="img_logo">
+
+                
+
+                <!-- Información de la empresa   -->
+                <h1>EXPERTORIA INFORMATICA</h1>
+                <p>Brindar la mejor calidad y atención a nuestros clientes para que se sientan seguros de nuestro trabajo para obtener un estatus alto. Además de expandirnos a más partes del país para seguir resolviendo los problemas tecnológicos de más empresas.</p>
+                <h2>SOBRE NOSOTROS</h2>
+                <p>Somos una empresa del área tecnológica que te ayuda a resolver los problemas de tu empresa, sin importar si tu empresa es una empresa grande o chica.</p>
+                <h3>¿QUIÉNES SOMOS?</h3>
+                <p>Somos una empresa del área tecnológica que te ayuda a resolver los problemas de tu empresa, sin importar si sea una empresa grande o chica, para así evitar la menor cantidad de perdidas monetarias y de tiempo.</p>
+                <h3>FILOSOFÍA</h3>
+                <p>Expandir nuestra empresa para seguir resolviendo los problemas tecnológicos de todas las empresas, para que tengan la mejor calidad de trabajo.</p>
+                <h3>MISIÓN</h3>
+                <p>Brindar la mejor calidad en nuestros trabajos para que tus procesos tecnológicos sean lo mejor para tu empresa.</p>
+                <h3>VALORES</h3>
+                <p>Nuestra empresa se basa en 4 valores principales para mantener nuestro estatus en alto:</p>
+                <h5>Transparencia</h5>
+                <p>El cliente siempre debe de saber que estamos haciendo para que pueda saber bien lo que realizamos y así no haya problemas.</p>
+                <h5>Trabajo en equipo</h5>
+                <p>Nuestros trabajadores crean una colaboración en la que comparten ideas y uniendo su esfuerzo y talento para poder resolver todo lo que se necesita.</p>
+                <h5>Respeto</h5>
+                <p>El respeto mutuo entre trabajador y empresa es lo mas importante para la sana convivencia y un ambiente ameno de trabajo. Además, el respeto a nuestros clientes es parte fundamental de la empresa.</p>
+                <h5>Responsabilidad</h5>
+                <p>Nos apegamos a los tiempos establecidos en los que hemos prometido para ayudarlo a resolver su problema tecnológico</p>
+
+                <br>
+                <hr>
+
+                <footer>
+                    <center>
+                    <p>Numero de teléfono: 3345108765</p>
+                    Corre electronico: <address>expertoria.informatica_inc@tecnologia.com</address>
+                    Visitanos en nuestra sucursal en: Av. Bellas Artes 333, Miravalle, Guadalajara, Jal.
+                    todos los derechos reservados <abbr title="Copyright">©</abbr> Expertoria Informatica Inc. 
+                    </center>
+                </footer>
+                
+            </div>
+            
+            <input type="radio" name="radio" id="radio2">
+            <div class="tab2">
+                <!-- Subpagina de creadores -->
+                <h2>CREADORES</h2>
+                <element id="erick"></element><img src="../../IMG/erick.jpeg" alt="Erick"></element>
+                <h3>Erick Alberto Vicente Villareal</h3>
+                <p>Director de redes</p>
+                <element id="cris"><img src="../../IMG/cristopher.jpeg" alt="Cris" id="cris"></element>
+                <h3>Cristopher García Carmona</h3>
+                <p>Programador de la solución</p>
+                <element id="mau"><img src="../../IMG/mauricio.jpeg" alt="Mau" id="mau" width="850px"></element>
+                <h3>Carlos Maurico Aguila Perez</h3>
+                <p>Director de diseño y desarrollo</p>
+
+                <br>
+                <hr>
+
+                <footer>
+                    <center>
+                    <p>Numero de teléfono: 3345108765</p>
+                    Corre electronico: <address>expertoria.informatica_inc@tecnologia.com</address>
+                    Visitanos en nuestra sucursal en: Av. Bellas Artes 333, Miravalle, Guadalajara, Jal.
+                    todos los derechos reservados <abbr title="Copyright">©</abbr> Expertoria Informatica Inc. 
+                    </center>
+                </footer>
+            </div>
+            
+            <input type="radio" name="radio" id="radio3">
+            <div class="tab3">
+                <!-- La problematica del proyecto(aun en desarrollo) -->
+                <h2>PROBLEMATICA</h2>
+                <embed src="../../PDF/Descripcion_de_Proyecto 4 Fabrica de productos lacteos y derivados.pdf#toolbar=0" type="application/pdf" width="840px" height="900px" />
+                <br>
+                <p>Prueba de pagina con los textos</p>
+
+                <br>
+                <hr>
+
+                <footer>
+                    <center>
+                    <p>Numero de teléfono: 3345108765</p>
+                    Corre electronico: <address>expertoria.informatica_inc@tecnologia.com</address>
+                    Visitanos en nuestra sucursal en: Av. Bellas Artes 333, Miravalle, Guadalajara, Jal.
+                    <p>todos los derechos reservados <abbr title="Copyright">©</abbr> Expertoria Informatica Inc.</p> 
+                    </center>
+                </footer>
+            </div>
+            
+            <input type="radio" name="radio" id="radio4">
+            <div class="tab4">
+                <h2>Documentos</h2>
+                <p>En este apartado, tu podras revisar los documentos y programas que hemos hecho anteriormente,
+                   aqui descargaras todo lo necesario para visualizar todo lo que hemos hecho para la empresa LACCT. 
+                </p>
+                <br>
+                <!-- contenedores con layout de display grid -->
+                <nav class="grid">
+                    <nav class="item item1"><a href="" title="descargar diagrama de gantt"><img src="../../IMG/grafico-de-gantt.png" alt="" width="100px"></a></nav>
+                    <nav class="item item2"><a href="" title="descargar la topologia lógica y física de Packet Tracer"><img src="../../IMG/redes-sociales.png" alt="" width="100px"></a></nav>
+                    <nav class="item item3"><a href="../../PDF/Empresa_Lactea-20220627T025324Z-001.zip" download="Empresa LACCT.zip" title="descargar el programa de Java"><img src="../../IMG/java.png" alt="" width="100px"></a></nav>
+                    <nav class="item item4"><a href="" title="descargar la pagina web"><img src="../../IMG/html-5.png" alt="" width="100px"></a></nav>
+                </nav>
+
+                <br>
+                <hr>
+
+                <footer>
+                    <center>
+                    <p>Numero de teléfono: 3345108765</p>
+                    Corre electronico: <address>expertoria.informatica_inc@tecnologia.com</address>
+                    Visitanos en nuestra sucursal en: Av. Bellas Artes 333, Miravalle, Guadalajara, Jal.
+                    todos los derechos reservados <abbr title="Copyright">©</abbr> Expertoria Informatica Inc. 
+                    </center>
+                </footer>
+            </div>
+
+            <input type="radio" name="radio" id="radio5">
+            <div class="tab5">
+                <h2>Contacto</h2>
+
+                <nav class="box"></nav>
+
+                <hr size="4px" color="black">
+
+                <nav class="was-validated">
+
+                    <nav class="mb-3">
+                    <input class="form-control" type="text" placeholder="expertoria.informatica_inc@tecnologia.com" aria-label="Disabled input example" disabled>
+                    </nav>
+
+                    <nav class="mb-3">
+                        <label for="validationTextarea" class="form-label">Asunto del correo</label>
+                        <textarea class="form-control is-invalid" id="validationTextarea" required></textarea>
+                    </nav>
+
+                    <nav class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-3" style="float: right;">Mandar correo</button>
+                    </nav>
+                </nav>
+
+                <br>
+                <br>
+                <hr size="4px" color="black">
+
+                <br>
+
+                <center>
+                    <p><strong>Tambien puedes visitarnos en nuestra sucursal en Guadalajara</strong></p>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.309100665732!2d-103.351925!3d20.616253999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b34af9eff521%3A0x7d85fe0cefdda66f!2sCONCEPTOS%20VISUALES..!5e0!3m2!1sen!2smx!4v1656301096784!5m2!1sen!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </center>
+
+                <hr size="4px" color="black">
+                <br>
+
+                <footer>
+                    <center>
+                    <p>Numero de teléfono: 3345108765</p>
+                    Corre electronico: <address>expertoria.informatica_inc@tecnologia.com</address>
+                    <p>Visitanos en nuestra sucursal en: Av. Bellas Artes 333, Miravalle, Guadalajara, Jal.</p>
+                    todos los derechos reservados <abbr title="Copyright">©</abbr> Expertoria Informatica Inc. 
+                    </center>
+                </footer>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+  
